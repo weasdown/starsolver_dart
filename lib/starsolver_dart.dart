06714d9f30dee90ab37702abee6b32291e0fb05f
weasdown <34220924+weasdown@users.noreply.github.com>
2026-07-26 17:59:51 +0100

    docs: update package docs

--- lib/starsolver_dart.dart
+++ lib/starsolver_dart.dart
@@ -1,11 +1,7 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
+/// Dart package providing the logic core for a Flutter implementation of starsolver.
 library;
 
 export 'src/board.dart';
 export 'src/cell.dart';
 export 'src/cell_group.dart';
 export 'src/shape.dart';
-
-// TODO: Export any libraries intended for clients of this package.
